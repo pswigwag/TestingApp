8eb124b7d01b4f32159d70522786cb8b798b0948
pspandey <pspandey01@gmail.com>
2018-12-24 16:35:25 +0530

    Add unit test class

---
+++ Unit Test/XCS-DemoUnitTestlogin.swift
@@ -0,0 +1,33 @@
+//
+//  XCS-DemoUnitTestlogin.swift
+//  XCS-DemoTests
+//
+//  Created by WigWag on 12/24/18.
+//  Copyright © 2018 Shashikant Jagtap. All rights reserved.
+//
+
+import XCTest
+
+class XCS_DemoUnitTestlogin: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
